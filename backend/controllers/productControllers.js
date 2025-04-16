5e71d64b44dfe452729c80fc1f6a7b627bf1f8aa
NiltonJe <nilcastro200420160@gmail.com>
2025-04-16 10:25:04 -0500

    toma puto III

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -41,27 +41,18 @@ class ProductController {
 
 
     async updateProduct(req, res) {
-        const { id } = req.params;
         try {
-            const { nombre, precio, descripcion } = req.body;
-            if (!nombre || precio === undefined || !descripcion) {
-                return res.status(400).json({ message: 'Todos los campos son requeridos' });
-            }
-
-            const updatedProduct = await productService.modifyProduct(id, { nombre, precio, descripcion });
-            
-            if (!updatedProduct) {
-                return res.status(404).json({ message: 'Producto no encontrado' });
-            }
-            
-            res.status(200).json(updatedProduct);
+            const {id} = req.params;
+            const {nombre, precio, descripcion} =req.body;
+            const updatedProduct = await productService.modifyProduct(id, { nombre, precio, descripcion});    
+            res.json(updatedProduct);
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Error al actualizar el producto' });
         }
     }
 
-    
+
     async deleteProduct(req, res) {
         try{
             const {id} = req.params;
